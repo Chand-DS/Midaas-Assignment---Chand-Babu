7238649ea728e96778ce80444682552aa8888031
chand babu <chandbabu184807@gmail.com>
2026-05-21 00:10:45 +0530

    Updated ExecutionLogController

--- src/main/java/com/example/MoonWalkRestro/controller/ExecutionLogController.java
+++ src/main/java/com/example/MoonWalkRestro/controller/ExecutionLogController.java
@@ -2,6 +2,8 @@ package com.example.MoonWalkRestro.controller;
 
 import com.example.MoonWalkRestro.entity.ExecutionLog;
 import com.example.MoonWalkRestro.repository.ExecutionLogRepository;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/logs")
 @RequiredArgsConstructor
+@Tag(name = "Execution Logs", description = "Audit and execution tracking APIs")
 public class ExecutionLogController {
 
     private final ExecutionLogRepository executionLogRepository;
